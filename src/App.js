b3911fc2ec10dc4dd2c3208da1ab7ff307059a61
amitshekatkar <shekatkaramit@gmail.com>
2019-12-25 20:37:13 +0530

    created skeleton.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Homepage } from './Components/HomePage';
+
+function App() {
+  return (
+    <div>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
